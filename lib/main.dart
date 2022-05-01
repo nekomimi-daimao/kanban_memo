3b74dbafa4638918c49e0c9774a0737d04bbecc7
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2022-05-01 15:31:12 +0900

    riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kanban_memo/memo_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
