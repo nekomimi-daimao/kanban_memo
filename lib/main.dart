91c9a92af674ed4cabe24562f2331169309b898c
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2022-05-07 22:56:50 +0900

    split main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,34 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:kanban_memo/db/dao.dart';
-import 'package:kanban_memo/model/memo/board_data.dart';
-import 'package:kanban_memo/model/memo/memo_data.dart';
-import 'package:kanban_memo/widget/kanban_board.dart';
 import 'package:kanban_memo/widget/main_page.dart';
 import 'package:kanban_memo/widget/testbed.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Dao().initialize();
-  await Testbed().init();
+  // await Testbed().init();
   // await Testbed().clear();
 
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MainPage();
-
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "kanban memo",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
         textTheme: GoogleFonts.sawarabiGothicTextTheme(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulHookConsumerWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() {
-    return _MyHomePageState();
-  }
-}
-
-class _MyHomePageState extends ConsumerState<MyHomePage> {
-  final boardListProvider = FutureProvider<List<BoardData>>((ref) async {
-    return Dao().allBoard();
-  });
-
-  final boardSelectedProvider =
-      StateProvider<BoardData>((ref) => BoardData.empty());
-
-  @override
-  Widget build(BuildContext context) {
-    AsyncValue<List<BoardData>> boards = ref.watch(boardListProvider);
-
-    return boards.when(
-        loading: () => const CircularProgressIndicator(),
-        error: (err, stack) => Text('Error: $err'),
-        data: (boards) {
-          List<Widget> cards = [];
-          if (boards.isEmpty) {
-            cards.add(const Text("Empty"));
-          } else {
-            for (var v in boards) {
-              cards.add(
-                ListTile(
-                  title: Text(v.title),
-                  onTap: () {
-                    ref.read(boardSelectedProvider.notifier).state = v;
-                  },
-                ),
-              );
-            }
-          }
-
-          return Scaffold(
-              appBar: AppBar(
-                title: Text(widget.title),
-                centerTitle: true,
-                actions: const [
-                  Icon(Icons.add),
-                  Icon(Icons.done),
-                ],
-              ),
-              body: KanbanBoard(ref.watch(boardSelectedProvider)),
-              drawer: Drawer(
-                child: ListView(
-                  children: cards,
-                ),
-              ));
-        });
   }
 }
--- lib/widget/main_page.dart
+++ lib/widget/main_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:kanban_memo/db/dao.dart';
 import 'package:kanban_memo/model/memo/board_data.dart';
 import 'package:kanban_memo/widget/kanban_board.dart';
+import 'package:kanban_memo/widget/dialog/dialog_edit_text.dart';
 
 class MainPage extends HookConsumerWidget {
   MainPage({Key? key}) : super(key: key);
@@ -18,51 +17,35 @@ class MainPage extends HookConsumerWidget {
   final boardSelectedProvider =
       StateProvider<BoardData>((ref) => BoardData.empty());
 
-  late BuildContext _buildContext;
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      title: "kanban memo",
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        textTheme: GoogleFonts.sawarabiGothicTextTheme(),
-      ),
-      home: _loadWait(ref),
-    );
-  }
-
-  Widget _loadWait(WidgetRef ref) {
     AsyncValue<List<BoardData>> boards = ref.watch(boardListProvider);
     return boards.when(
       loading: () => const CircularProgressIndicator(),
       error: (err, stack) => Text('Error: $err'),
       data: (boards) {
-        List<Widget> drawerItem = _createDrawerItem(ref, boards);
-        return _createScaffold(ref, drawerItem);
+        var drawerItem = _createDrawerItem(ref, context, boards);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(ref.watch(boardSelectedProvider).title),
+            centerTitle: true,
+            actions: const [
+              Icon(Icons.settings_applications_rounded),
+            ],
+          ),
+          body: KanbanBoard(ref.watch(boardSelectedProvider)),
+          drawer: Drawer(
+            child: ListView(
+              children: drawerItem,
+            ),
+          ),
+        );
       },
     );
   }
 
-  Scaffold _createScaffold(WidgetRef ref, List<Widget> drawerItem) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(ref.watch(boardSelectedProvider).title),
-        centerTitle: true,
-        actions: const [
-          Icon(Icons.settings_applications_rounded),
-        ],
-      ),
-      body: KanbanBoard(ref.watch(boardSelectedProvider)),
-      drawer: Drawer(
-        child: ListView(
-          children: drawerItem,
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _createDrawerItem(WidgetRef ref, List<BoardData> boards) {
+  List<Widget> _createDrawerItem(
+      WidgetRef ref, BuildContext context, List<BoardData> boards) {
     List<Widget> drawerItem = [];
     if (boards.isEmpty) {
       drawerItem.add(const Text("No Board"));
@@ -83,12 +66,13 @@ class MainPage extends HookConsumerWidget {
       key: const Key("add"),
       title: const Icon(Icons.add_box_rounded),
       onTap: () async {
-        var boardName = await _addBoardDialog();
+        var boardName = await _addBoardDialog(context);
         if (boardName == null || boardName.isEmpty) {
           return;
         }
         var newBoard = BoardData.create(boardName);
         await Dao().putBoard(newBoard);
+        ref.refresh(boardListProvider);
       },
     );
     drawerItem.add(addTile);
@@ -96,46 +80,14 @@ class MainPage extends HookConsumerWidget {
     return drawerItem;
   }
 
-  Future<String?> _addBoardDialog() async {
-    var textEditingController = TextEditingController();
+  Future<String?> _addBoardDialog(BuildContext context) async {
+    var builder = EditTextDialog.builder("Add Board");
+    builder.submit = "Create";
 
-    var inputResult = await showDialog<String>(
-      context: _buildContext,
-      barrierDismissible: true,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Add Board"),
-          content: Column(
-            children: [
-              TextFormField(
-                controller: textEditingController,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (v) {
-                  if (v == null || v.isEmpty) {
-                    return "Empty!";
-                  }
-                  return null;
-                },
-              ),
-            ],
-          ),
-          actions: [
-            OutlinedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Cancel"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(textEditingController.text);
-              },
-              child: const Text("Create"),
-            ),
-          ],
-        );
-      },
-    );
-    return inputResult;
+    return showDialog<String>(
+        context: context,
+        builder: (_) {
+          return builder.build();
+        });
   }
 }
