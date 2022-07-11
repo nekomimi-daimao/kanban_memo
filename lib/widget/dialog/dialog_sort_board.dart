52bb5b2eefe53c636b0dd526a0e66ad0c584e388
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2022-07-12 02:07:59 +0900

    convert SortBoardDialog StatefulWidget

--- lib/widget/dialog/dialog_sort_board.dart
+++ lib/widget/dialog/dialog_sort_board.dart
@@ -5,10 +5,13 @@ import 'package:collection/collection.dart';
 import 'package:kanban_memo/model/memo/board_data.dart';
 import 'package:kanban_memo/widget/dialog/enum/enum_edit_result.dart';
 
-class SortBoardDialog extends StatelessWidget {
+class SortBoardDialog extends StatefulWidget {
   final List<BoardData> boardData;
 
   const SortBoardDialog.create(this.boardData, {Key? key}) : super(key: key);
+
+  @override
+  State<SortBoardDialog> createState() => _SortBoardDialogState();
 
   Future<EditResultType?> show(BuildContext context) {
     return showDialog<EditResultType>(
@@ -18,12 +21,14 @@ class SortBoardDialog extends StatelessWidget {
           return this;
         });
   }
+}
 
+class _SortBoardDialogState extends State<SortBoardDialog> {
   @override
   Widget build(BuildContext context) {
     var list = ReorderableListView(
         shrinkWrap: true,
-        children: boardData
+        children: widget.boardData
             .map((e) => ListTile(
                   key: Key('$e.id'),
                   title: Text(e.title),
@@ -33,8 +38,10 @@ class SortBoardDialog extends StatelessWidget {
           if (oldIndex < newIndex) {
             newIndex -= 1;
           }
-          var item = boardData.removeAt(oldIndex);
-          boardData.insert(newIndex, item);
+          setState(() {
+            var item = widget.boardData.removeAt(oldIndex);
+            widget.boardData.insert(newIndex, item);
+          });
         });
 
     return AlertDialog(
@@ -52,7 +59,11 @@ class SortBoardDialog extends StatelessWidget {
         ),
         ElevatedButton(
           onPressed: () {
-            boardData.whereNotNull().toList().asMap().forEach((key, value) {
+            widget.boardData
+                .whereNotNull()
+                .toList()
+                .asMap()
+                .forEach((key, value) {
               value.index = key;
             });
             Navigator.of(context).pop(EditResultType.submit);
